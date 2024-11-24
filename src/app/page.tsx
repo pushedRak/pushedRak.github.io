c81732306ec1937c5b543838b78714a6c0aaf1bb
손민락 <dungguk@naver.com>
2024-11-25 01:28:26 +0900

    design: Home 페이지 CSS 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,19 @@ const Home: React.FC = () => {
   
   return (
     <div className={styles.container}>
-      <section className={styles.posts}>
-        <h2>최근 포스트</h2>
-        {posts.map((post) => (
-          <Link href={`/posts/${post.slug}`} key={post.slug}>
+      <section>
+        <h2 className={styles.title}>최근 포스트</h2>
+        <div className={styles.posts}>
+          {posts.map((post) => (
+            <Link href={`/posts/${post.slug}`} key={post.slug} className={styles.link}>
             <article className={styles.post}>
               <h3>{post.metadata.title}</h3>
               <p>{post.metadata.description}</p>
               <time>{post.metadata.date}</time>
             </article>
           </Link>
-        ))}
+          ))}
+        </div>
         <Link href="/posts" className={styles.more}>
           모든 포스트 보기 →
         </Link>
