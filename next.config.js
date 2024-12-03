d58cffabd68b70a85a247a8d2f923cb6c17193e5
손민락 <dungguk@naver.com>
2024-12-03 18:10:48 +0900

    feat: strictmode false

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   output: 'export',
   images: {
     unoptimized: true,
@@ -7,4 +8,4 @@ const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/rak-blog' : '',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
